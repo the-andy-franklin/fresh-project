320dccb4a53583c5acf33b4543bc252f7effe9e1
Andy <the.andy.franklin@gmail.com>
2023-06-16 19:26:57 -0700

    spacing better

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,33 +1,29 @@
 import { Head } from "$fresh/runtime.ts";
 import { tw } from "twind";
-import { Layout } from "../components/Layout.tsx";
 
 export default function Home() {
   return (
-    <>
+    <div className={tw`w-screen h-screen flex flex-col`}>
       <Head>
         <title>Fresh App</title>
         <link rel="stylesheet" href="/index.css" />
-        <link
-          rel="manifest"
-          href="/manifest.json"
-        />
-
+        <link rel="manifest" href="/manifest.json" />
         <script src="/register-sw.js" />
       </Head>
-      <Layout>
-        <div
-          className={tw`
-        w-full h-screen
-        bg-gray-800 text-green-300
-        flex items-center justify-center
-        `}
-        >
-          <p className={tw`text-4xl font-bold p-4 text-rainbow`}>
-            Hello World!
-          </p>
-        </div>
-      </Layout>
-    </>
+      <nav
+        className={tw`bg-gradient-linear from-pink-500 via-green-500 to-blue-500 text-white px-6 py-4`}
+      >
+        <span className={tw`text-5xl font-semibold text-white`}>
+          Mezo
+        </span>
+      </nav>
+      <div
+        className={tw`w-full flex-grow bg-gray-800 text-green-300 flex items-center justify-center`}
+      >
+        <p className={tw`text-4xl font-bold p-4 text-rainbow`}>
+          Hello World!
+        </p>
+      </div>
+    </div>
   );
 }
