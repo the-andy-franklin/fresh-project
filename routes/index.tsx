0a841feedfee8bbe67b048630d1b7341bc702000
Andy <the.andy.franklin@gmail.com>
2023-06-16 18:04:50 -0700

    trying to setup service-worker.js

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,23 +1,66 @@
 import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { tw } from "twind";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
+        <link rel="stylesheet" href="/index.css" />
+        <link
+          crossOrigin="use-credentials"
+          rel="manifest"
+          href="/manifest.json"
         />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
+
+        <script type="module">
+          import
+          'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate/dist/pwa-update.js';
+          const el = document.createElement('pwa-update');
+          document.body.appendChild(el);
+        </script>
+
+        <script>
+          if ('serviceWorker' in navigator){" "}
+          {self.addEventListener("load", function () {
+            navigator.serviceWorker.register("/pwa-sw.js").then(
+              function (registration) {
+                console.log(
+                  "ServiceWorker registration successful with scope: ",
+                  registration.scope,
+                );
+              },
+              function (err) {
+                console.log("ServiceWorker registration failed: ", err);
+              },
+            );
+          })}
+        </script>
+      </Head>
+      <nav
+        className={tw`
+          bg-gradient-linear 
+          from-pink-500
+          via-green-500
+          to-blue-500
+          text-white
+          px-6 py-4
+        `}
+      >
+        <span className={tw`text-5xl font-semibold text-white`}>
+          Mezo
+        </span>
+      </nav>
+      <div
+        className={tw`
+          w-full h-screen
+          bg-gray-800 text-green-300
+          flex items-center justify-center
+        `}
+      >
+        <p className={tw`text-4xl font-bold p-4 text-rainbow`}>
+          Hello World!
         </p>
-        <Counter start={3} />
       </div>
     </>
   );
--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,13 @@ import type { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-linear": "linear-gradient(135deg, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
+      },
+    },
+  },
 } as Options;
