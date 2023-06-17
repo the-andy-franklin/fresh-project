470c1d376a36afbbe5982725b8900b2c565b32a5
Andy <the.andy.franklin@gmail.com>
2023-06-16 20:27:13 -0700

    install button working

---
+++ islands/InstallPWAButton.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "preact/hooks";
+import { tw } from "twind";
+
+interface BeforeInstallPromptEvent extends Event {
+  prompt?: () => void;
+  userChoice?: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
+}
+
+const InstallPWAButton = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState<
+    BeforeInstallPromptEvent | null
+  >(null);
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    addEventListener("beforeinstallprompt", (e: BeforeInstallPromptEvent) => {
+      e.preventDefault();
+      setDeferredPrompt(e as BeforeInstallPromptEvent);
+    });
+
+    return () => {
+      removeEventListener("beforeinstallprompt", (e) => {});
+    };
+  }, []);
+
+  const onClick = () => {
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt?.();
+
+    deferredPrompt.userChoice?.then(() => {
+      setDeferredPrompt(null);
+    });
+  };
+
+  return (
+    <button
+      ref={btnRef}
+      onClick={onClick}
+      id="install-button"
+      className={tw`border rounded px-4 py-2 text-rainbow`}
+    >
+      Install
+    </button>
+  );
+};
+
+export default InstallPWAButton;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
 import { tw } from "twind";
+import InstallPWAButton from "../islands/InstallPWAButton.tsx";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="stylesheet" href="/index.css" />
         <link rel="manifest" href="/manifest.json" />
         <script src="/register-sw.js" />
+        <script src="/install.js" defer />
       </Head>
       <nav
         className={tw`bg-gradient-linear from-pink-500 via-green-500 to-blue-500 text-white px-6 py-4`}
@@ -18,11 +20,12 @@ export default function Home() {
         </span>
       </nav>
       <div
-        className={tw`w-full flex-grow bg-gray-800 text-green-300 flex items-center justify-center`}
+        className={tw`w-full flex flex-col flex-grow bg-gray-800 text-green-300 flex items-center justify-center`}
       >
         <p className={tw`text-4xl font-bold p-4 text-rainbow`}>
           Hello World!
         </p>
+        <InstallPWAButton />
       </div>
     </div>
   );
