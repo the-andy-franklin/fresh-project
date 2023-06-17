cc26d6db39375226ca9fa54d720d1f9b1584e44f
Andy <the.andy.franklin@gmail.com>
2023-06-17 09:18:14 -0700

    100% lighthouse score in all categories

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,6 +5,11 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="description"
+          content="Unlock Your Personalized Cannabinoid Profile"
+        />
+        <meta name="theme-color" content="#1F2937" />
         <title>Fresh App</title>
         <link rel="stylesheet" href="/index.css" />
         <link rel="manifest" href="/manifest.json" />
