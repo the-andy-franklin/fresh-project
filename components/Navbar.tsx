8060e361faee73eb0831b512b10b579298624753
Andy <the.andy.franklin@gmail.com>
2023-06-20 09:50:07 -0700

    removed extraneous tw references

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,9 @@
-import { tw } from "twind";
 import type { JSX } from "preact";
 
 type NavProps = JSX.IntrinsicElements["nav"];
 const Nav = (props: NavProps) => (
   <nav
-    className={tw`bg-gradient-linear from-purple-500 via-green-500 to-blue-500 text-white px-6 py-4`}
+    className="bg-gradient-linear from-purple-500 via-green-500 to-blue-500 text-white px-6 py-4"
     {...props}
   />
 );
@@ -12,7 +11,7 @@ const Nav = (props: NavProps) => (
 type SpanProps = JSX.IntrinsicElements["span"];
 const Span = (props: SpanProps) => (
   <span
-    className={tw`text-5xl font-semibold text-white text-outline`}
+    className="text-5xl font-semibold text-white text-outline"
     {...props}
   />
 );
