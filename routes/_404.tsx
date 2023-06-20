096961630a5ca6a3e01fa62231077aec53dfb34e
Andy <the.andy.franklin@gmail.com>
2023-06-20 10:48:40 -0700

    deno kv stuff removed

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,31 +1,9 @@
 import type { HandlerContext, PageProps } from "$fresh/server.ts";
-import { z } from "zod";
-
-const outSchema = z.array(
-  z.object({}).passthrough(),
-);
-
-type Out = z.infer<typeof outSchema>;
 
 export const handler = async (req: Request, ctx: HandlerContext) => {
-  const route = req.url.slice(req.url.lastIndexOf("/") + 1);
-  const kv = await Deno.openKv();
-
-  await kv.set([404], route);
-
-  const iter = kv.list({ prefix: [] });
-  const entries = [];
-  for await (const entry of iter) {
-    entries.push(entry);
-    await kv.delete(entry.key);
-  }
-
-  const out = outSchema.parse(entries);
-  return await ctx.render(out);
+  return await ctx.render();
 };
 
-export default function NotFoundPage(
-  props: PageProps<Out>,
-) {
-  return <pre>{JSON.stringify(props.data, null, 2)}</pre>;
+export default function NotFoundPage({ url }: PageProps) {
+  return <p>No page found for ${url.pathname}</p>;
 }
