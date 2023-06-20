5c10726e07d4be4b2d49e8313e00c56f7746bcc6
Andy <the.andy.franklin@gmail.com>
2023-06-20 10:01:46 -0700

    offline.css works now

--- static/sw.js
+++ static/sw.js
@@ -3,8 +3,8 @@ importScripts(
 );
 
 const CACHE = "pwabuilder-page";
-
 const offlineFallbackPage = "offline.html";
+const offlineFallbackCss = "offline.css";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -14,8 +14,9 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage)),
+    caches.open(CACHE).then((cache) =>
+      cache.addAll([offlineFallbackPage, offlineFallbackCss])
+    ),
   );
 });
 
@@ -25,21 +26,26 @@ if (workbox.navigationPreload.isSupported()) {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.mode === "navigate") {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
+          if (preloadResp) return preloadResp;
 
-        if (preloadResp) {
-          return preloadResp;
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
         }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
+      })(),
+    );
+  } else if (event.request.url.endsWith(".css")) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      }),
+    );
   }
 });
