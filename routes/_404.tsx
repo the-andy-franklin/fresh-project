7b493a6ba9cde01f9f529519308ce4c2c94d0db8
Andy <the.andy.franklin@gmail.com>
2023-09-04 13:12:11 -0700

    404 opted out of layouts

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,9 +1,9 @@
-import type { HandlerContext, PageProps } from "$fresh/server.ts";
-
-export const handler = async (req: Request, ctx: HandlerContext) => {
-  return await ctx.render();
-};
+import type { PageProps, RouteConfig } from "$fresh/server.ts";
 
 export default function NotFoundPage({ url }: PageProps) {
   return <p>No page found for {url.pathname}</p>;
 }
+
+export const config: RouteConfig = {
+  skipInheritedLayouts: true,
+};
