d7f7a92a89eba02eda5f46c2d732cffd8c89b699
Andy <the.andy.franklin@gmail.com>
2023-09-09 15:50:41 -0700

    upgraded to fresh1.4.3

--- main.ts
+++ main.ts
@@ -3,14 +3,11 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-/// <reference lib="deno.unstable" />
 
 import "$std/dotenv/load.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.ts";
-
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, config);
